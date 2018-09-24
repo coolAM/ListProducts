efcc88cde8eed9457dd3d46602a40ec26aae5411
CoolAM <mischanton@gmail.com>
2018-09-24 11:55:07 +0300

    Initial commit

---
+++ my-app/src/Container/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    render() {
+        return (
+
+            <BrowserRouter>
+            <div>
+
+        </div>
+        </BrowserRouter>
+    );
+    }
+}
+
+export default App;
